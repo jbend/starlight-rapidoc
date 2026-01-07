cc9987d6308b8be470401d3479169f6bd726e819
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2026-01-07 10:01:18 -0800

    Updating type declatations

---
+++ packages/starlight-rapidoc/locals.d.ts
@@ -0,0 +1,5 @@
+declare namespace App {
+  interface Locals {
+    starlightRoute: import('@astrojs/starlight/utils/routing').StarlightRouteData;
+  }
+}
---
+++ packages/starlight-rapidoc/virtual.d.ts
@@ -0,0 +1,4 @@
+declare module "virtual:starlight-rapidoc-config" {
+  const StarlightRapidocConfig: import("./libs/config").StarlightRapidocConfig;
+  export default StarlightRapidocConfig;
+}
